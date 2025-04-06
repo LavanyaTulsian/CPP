e41cd0013cb4bb5c316ffacedb5526516a6dd4cc
Lavanya Tulsian <tulsian2lavanya@gmail.com>
2025-04-06 17:47:46 +0530

    Introduction and basic linking

--- 001 Introduction/Intro/Main.cpp
+++ 001 Introduction/Intro/Main.cpp
@@ -1,8 +1,14 @@
 #include<iostream>
 using namespace std;
 
+void Log(const char* message);//declaration--> gets linked by the linker to the definition in Log.cpp
+
 int main()
 {
-	cout << "Hello World!" << endl;
-	cin.get();
+	Log("Hello world");
+	cin.get();//pauses the program and waits for enter input to  continue the program
 }
+
+
+//For Linking the function name, no. of arguments and return type of definition and declaration needs to be same
+//Any of the three being different results in a different function and hence is not the same function with a different function ID
